2666872822470ec8a08ac22c2dac06e76049b4dd
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-13 22:07:45 +0600

    responsive topnav part

--- src/components/home/topNav/Index.jsx
+++ src/components/home/topNav/Index.jsx
@@ -5,28 +5,65 @@ import { icons } from '../../../helpers/iconProvider'
 const TopNav = () => {
   const { topNav } = icons
 
-
   return (
     <section className='bg-secondary_700'>
       <Container>
-        <div className="flex justify-between items-center">
-          <h2 className='label3 text-gray_00 py-4'>Welcome to Clicon online eCommerce store. </h2>
+        <div className="
+          flex 
+          flex-col sm:flex-row 
+          justify-between 
+          items-center 
+          gap-y-2 sm:gap-y-0
+          py-2 sm:py-0
+        ">
+          
+          {/* left text */}
+          <h2 className='
+            label3 
+            text-gray_00 
+            py-2 sm:py-4 
+            text-center sm:text-left
+            text-xs sm:text-sm md:text-base
+          '>
+            Welcome to Clicon online eCommerce store.
+          </h2>
 
-          <div className="flex items-center gap-x-2">
-            <h2 className='body_sm_400 text-gray_00'>Follow us:</h2>
+          {/* right section */}
+          <div className="
+            flex 
+            items-center 
+            gap-x-2 sm:gap-x-3
+            flex-wrap
+            justify-center sm:justify-end
+          ">
+            <h2 className='
+              body_sm_400 
+              text-gray_00 
+              text-xs sm:text-sm
+            '>
+              Follow us:
+            </h2>
+
             <div className="flex gap-x-2">
-
               {topNav.map((item) => (
-                <span key={item.id} className='cursor-pointer'>
-                  {<item.name size={18} color='#ffffff' />}
-                  </span>
+                <span 
+                  key={item.id} 
+                  className='cursor-pointer'
+                >
+                  <item.name 
+                    size={16} 
+                    className="sm:w-[18px] sm:h-[18px]" 
+                    color='#ffffff' 
+                  />
+                </span>
               ))}
             </div>
-            </div>
+
           </div>
+        </div>
       </Container>
     </section>
   )
 }
 
-export default TopNav
+export default React.memo(TopNav)
