25a38fc158fb6a4ee5087fc00804d42702c7500d
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-13 22:20:26 +0600

    rechek responsive

--- src/components/home/topNav/Index.jsx
+++ src/components/home/topNav/Index.jsx
@@ -1,69 +1,25 @@
-import React from 'react'
-import Container from '../../commonComponent/Container'
-import { icons } from '../../../helpers/iconProvider'
+<section className='bg-secondary_700'>
+  <Container>
+    <div className="flex flex-col sm:flex-row justify-between items-center gap-y-2 py-2">
 
-const TopNav = () => {
-  const { topNav } = icons
+      <h2 className='label3 text-gray_00 text-xs sm:text-sm md:text-base text-center sm:text-left'>
+        Welcome to Clicon online eCommerce store.
+      </h2>
 
-  return (
-    <section className='bg-secondary_700'>
-      <Container>
-        <div className="
-          flex 
-          flex-col sm:flex-row 
-          justify-between 
-          items-center 
-          gap-y-2 sm:gap-y-0
-          py-2 sm:py-0
-        ">
-          
-          {/* left text */}
-          <h2 className='
-            label3 
-            text-gray_00 
-            py-2 sm:py-4 
-            text-center sm:text-left
-            text-xs sm:text-sm md:text-base
-          '>
-            Welcome to Clicon online eCommerce store.
-          </h2>
+      <div className="flex items-center gap-x-3 flex-wrap justify-center sm:justify-end">
+        <h2 className='body_sm_400 text-gray_00 text-xs sm:text-sm'>
+          Follow us:
+        </h2>
 
-          {/* right section */}
-          <div className="
-            flex 
-            items-center 
-            gap-x-2 sm:gap-x-3
-            flex-wrap
-            justify-center sm:justify-end
-          ">
-            <h2 className='
-              body_sm_400 
-              text-gray_00 
-              text-xs sm:text-sm
-            '>
-              Follow us:
-            </h2>
-
-            <div className="flex gap-x-2">
-              {topNav.map((item) => (
-                <span 
-                  key={item.id} 
-                  className='cursor-pointer'
-                >
-                  <item.name 
-                    size={16} 
-                    className="sm:w-[18px] sm:h-[18px]" 
-                    color='#ffffff' 
-                  />
-                </span>
-              ))}
-            </div>
-
-          </div>
+        <div className="flex gap-x-2">
+          {topNav.map((item) => (
+            <span key={item.id} className='cursor-pointer'>
+              <item.name size={16} className="sm:w-[18px] sm:h-[18px]" color="#fff" />
+            </span>
+          ))}
         </div>
-      </Container>
-    </section>
-  )
-}
+      </div>
 
-export default React.memo(TopNav)
+    </div>
+  </Container>
+</section>
