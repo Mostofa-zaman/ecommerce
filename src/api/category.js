24ff9bd62f4eda669d1309820a60e23e7085f97f
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-24 23:54:46 +0600

    adjusting add to cart number

--- src/api/category.js
+++ src/api/category.js
@@ -1,5 +1,8 @@
 import instance from "./axios";
 
+/**
+ * Get all categories
+ */
 const getCategory = async () => {
   try {
     const { data, status, statusText } = await instance.get(
@@ -12,6 +15,10 @@ const getCategory = async () => {
   }
 };
 
+
+/**
+ * Get all products
+ */
 const getAllProduct = async () => {
   try {
     const { data, status, statusText } = await instance.get("/products");
@@ -22,27 +29,45 @@ const getAllProduct = async () => {
   }
 };
 
-// get product by category
 
+/**
+ * Get product by category
+ */
 const getProductbyCategory = async (categroyName = "furniture") => {
   try {
-    const { data, status, statusText } = await instance.get(`/products/category/${categroyName}`  );
-   
-    return { data, status, statusText };
-  } catch (error) {
-    console.error("product network request failed ", error);
-  }
-};
-// best product using limit & skip
+    const { data, status, statusText } = await instance.get(
+      `/products/category/${categroyName}`
+    );
 
-const getProductbyLimit = async ( limit= 9) => {
-  try {
-    const { data, status, statusText } = await instance.get(`/products?limit=12&skip=0`  );
-   console.log(data)
     return { data, status, statusText };
   } catch (error) {
     console.error("product network request failed ", error);
-    throw error ;
   }
 };
-export { getCategory, getAllProduct, getProductbyCategory, getProductbyLimit };
+
+
+/**
+ * Get best products using limit & skip
+ */
+const getProductbyLimit = async (limit = 9) => {
+  try {
+    const { data, status, statusText } = await instance.get(
+      `/products?limit=12&skip=0`
+    );
+
+    console.log(data);
+
+    return { data, status, statusText };
+  } catch (error) {
+    console.error("product network request failed ", error);
+    throw error;
+  }
+};
+
+
+export {
+  getCategory,
+  getAllProduct,
+  getProductbyCategory,
+  getProductbyLimit,
+};
