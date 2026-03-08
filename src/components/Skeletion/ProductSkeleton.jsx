445836f688c8def8ca5b1fb4d9f017697079159a
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-03-08 23:51:02 +0600

    Make ProductSkeleton fully responsive

--- src/components/Skeletion/ProductSkeleton.jsx
+++ src/components/Skeletion/ProductSkeleton.jsx
@@ -1,27 +1,26 @@
 const ProductSkeleton = () => {
   return (
-    <div className="p-4 border border-gray_50 w-full rounded-lg animate-pulse">
-      <div className="flex flex-col gap-y-3">
-        
+    <div className="p-4 border border-gray-50 w-full rounded-lg animate-pulse">
+      <div className="flex flex-col gap-3">
         {/* HOT badge */}
-        <div className="w-12 h-6 bg-gray-200 rounded-md"></div>
+        <div className="w-1/4 sm:w-1/5 h-6 bg-gray-200 rounded-md"></div>
 
         {/* Image */}
         <div className="w-full aspect-[4/3] bg-gray-200 rounded-md"></div>
 
         {/* Rating */}
-        <div className="flex items-center gap-x-2 mt-2">
-          <div className="w-20 h-4 bg-gray-200 rounded-md"></div>
-          <div className="w-8 h-4 bg-gray-200 rounded-md"></div>
+        <div className="flex items-center gap-2 mt-2">
+          <div className="w-1/2 sm:w-1/3 h-4 bg-gray-200 rounded-md"></div>
+          <div className="w-1/6 sm:w-1/5 h-4 bg-gray-200 rounded-md"></div>
         </div>
 
         {/* Title */}
         <div className="w-full h-4 bg-gray-200 rounded-md"></div>
 
         {/* Price */}
-        <div className="flex gap-x-3">
-          <div className="w-12 h-4 bg-gray-200 rounded-md"></div>
-          <div className="w-14 h-4 bg-gray-200 rounded-md"></div>
+        <div className="flex gap-3">
+          <div className="w-1/4 sm:w-1/6 h-4 bg-gray-200 rounded-md"></div>
+          <div className="w-1/3 sm:w-1/4 h-4 bg-gray-200 rounded-md"></div>
         </div>
       </div>
     </div>
