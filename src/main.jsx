6e5726147906e83ee55050e159d59203af352780
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-03-19 23:57:14 +0600

    rape app of provider

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,15 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,
   </StrictMode>,
 )
